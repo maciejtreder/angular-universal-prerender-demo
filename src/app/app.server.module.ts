34664765f34727f88374a149c85cff0462d1ce96
Maciej Treder <contact@maciejtreder.com>
2020-11-15 00:14:25 +0100

    a

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,6 +3,8 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ContentService } from './content.service';
+import { ContentServerService } from './content-server.service';
 
 @NgModule({
   imports: [
@@ -10,5 +12,8 @@ import { AppComponent } from './app.component';
     ServerModule,
   ],
   bootstrap: [AppComponent],
+  providers: [
+    {provide: ContentService, useClass: ContentServerService}
+  ]
 })
 export class AppServerModule {}
--- src/app/content-server.service.ts
+++ src/app/content-server.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import * as fs from 'fs';
+import { Observable } from 'rxjs';
+import { Realization } from './model/realization.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,15 @@ import { Injectable } from '@angular/core';
 export class ContentServerService {
 
   constructor() { }
+
+
+  public getRealizations(): Observable<Realization[]> {
+    return new Observable(subject => {
+      fs.readFile(`${process.cwd()}/src/assets/realizations.json`, 'utf8', (err, data) => {
+        subject.next(JSON.parse(data));
+        subject.complete();
+      });
+    })
+    // return this.http.get<Realization[]>('/angular-universal-prerender-demo/assets/realizations.json');
+  }
 }
